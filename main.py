3e153a7721f1174db24663c6339899ebe3fb69f9
longbow33 <seitzsebastian24@web.de>
2021-12-23 21:12:41 +0100

    Used the right Profile used at Searwing.org and modified the Code to use the off-center neutral fiber

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-profile = np.genfromtxt("NACA0012.txt")
-
+profile = np.genfromtxt("S3010.txt")
 def gemoetricalMomentOfInertia(D): #IN MICROMETER!!
-    #calculate to meter
-    D = D*10**-6
+    #calculate to millimeter
+    D = D*10**-3
     W = (math.pi*D**4)/64
     return W
 
 def parallelAxesPortion(D,y):
+    D = D*10**-3 #to millimeter
     R = D/2
     A = math.pi*R**2
-    PAP = (y**2)*A
+    y = y*10**-3 #to millimerter
+    PAP = abs(y**2)*A
     return PAP
 
-def getMomentofInertia (profile,D):
+def geometricCenterOfGravity(profile):
+    return (sum(profile[:,1])/len(profile[:,1]))
+
+def getMomentofInertia (profile,D,COG):
     gesMomentofInnertia = []
     gesMomentofBendingResistance = []
 
     for i in range(len(profile[:,0])):
         gesMom = gemoetricalMomentOfInertia(D)+parallelAxesPortion(D,profile[i,1])
         gesMomentofInnertia.append(gesMom)
-        if profile[i,1] > 0 :
-            W = gesMom/profile[i,1]
+        if abs(profile[i,1]) > 0 :
+            W = gesMom/abs((profile[i,1]-COG)*10**-3)
             gesMomentofBendingResistance.append(W)
 
     return (gesMomentofInnertia,gesMomentofBendingResistance)
 
+COG = geometricCenterOfGravity(profile)
 
-I,W =  getMomentofInertia(profile,20)
+I,W =  getMomentofInertia(profile,20,COG)
 
 
 plt.plot(profile[:,0],profile[:,1])
 plt.xlim(0,1)
 plt.ylim(-.5,.5)
-plt.text(0.1,0.3,"Geometrical Moment of Inertia = "+ str(round(sum(I,2)))+" m⁴",fontsize = 15)
-plt.text(0.1,0.2,"Bending Resistance Moment = "+ str(round(sum(W,2)))+" m³",fontsize = 15)
+plt.text(0.1,0.3,"I = "+ str(round(sum(I),8))+" mm⁴",fontsize = 15)
+plt.text(0.1,0.2,"W = "+ str(round(sum(W),8))+" mm³",fontsize = 15)
 plt.xlabel("Meters")
 plt.ylabel("Meters")
 plt.show()
